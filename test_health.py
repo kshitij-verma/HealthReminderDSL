c959c48944d12fc0d1d8d272534b5a0426b6ead1
kshitij-verma <72070067+kshitij-verma@users.noreply.github.com>
2026-04-22 02:00:58 +0200

    added conflict test

--- test_health.py
+++ test_health.py
@@ -14,3 +14,12 @@ def test_medication_parsing():
     # 4. ASSERT (The actual test)
     # We expect the first medication in the list to be named "Aspirin"
     assert model.medications[0].name == "Aspirin"
+
+def test_dynamic_conflict_parsing():
+    meta_model = metamodel_from_file('health.tx')
+    example = "medication A medication B conflict A with B within 45 min message 'Danger'"
+    model = meta_model.model_from_str(example)
+    
+    # This verifies your new grammar logic works!
+    assert model.conflicts[0].time == 45
+    assert model.conflicts[0].medA.name == "A"
